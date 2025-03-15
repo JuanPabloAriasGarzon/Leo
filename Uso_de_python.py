0e117f881b2c0fd56a45c76b16faeae8fbdeb753
JuanPabloAriasGarzon <jupaga402@gmail.com>
2025-03-14 19:17:25 -0500

    introduccion a python

---
+++ Uso_de_python.py
@@ -0,0 +1,27 @@
+# Autor: Juan Pablo 
+print("Estudiando python")
+
+#Definicion de variables
+'''
+Reglas: 
+Python es sensible a mayusculas y minusculas
+No se puede usar espacios
+No se puede usar palabras reservadas
+No es conveniente usar numeros como nombres de variables'''
+x=40
+y=15
+
+print(x+y)
+print(type(x))
+
+# <class 'int'>: significa que x es un numero entero
+nombre="Juan Pablo"
+print(nombre)
+print(type(nombre))
+#<class 'str'>: significa que la variable nombre es un string(cadena)
+print(type(x))
+x=4.8
+print(type(x))
+#<class 'float'>: significa que la variable es un flotante (numero decimal)
+
+print(x , nombre)
