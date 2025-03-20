0baceb1336c81387e273e485bb4612a7d4565fec
JuanPabloAriasGarzon <jupaga402@gmail.com>
2025-03-19 19:31:46 -0500

    tipos de operadores en python

---
+++ tipos_de_operadores.py
@@ -0,0 +1,118 @@
+'''
+Operadores aritmeticos
+'''
+
+# Suma
+#x=2+6
+#print(x)
+
+# Resta
+#y=8-2
+#print(y)
+
+# Multiplicacion
+#print(2*25)
+
+# Division
+'''para python la division siempre retorna un float'''
+#divide=25/5
+#print(divide)
+#print(type(divide))
+
+#Division piso (floor)
+'''La division piso me devuelve el numero entero mas proximo hacia abajo'''
+
+#print(10/3)
+#print(10//3)
+
+#print(4/3)
+#print(4//3)
+
+#print(14.5/3)
+#print(14.5//3)
+
+'''Prueba realizar la division piso 8//3 y comparala con la division normal 8/3'''
+
+#print(8/-3)
+#print(8//-3)
+
+#Modulo
+# w=20/3
+# y=20%3
+
+# print(w)
+# print(y)
+
+# print(type(y))
+
+#Potencia
+# print(2**3)
+
+'''Operadores de comparacion'''
+
+
+#Es igual a
+# print("x"=="X")
+# print(3==3.0)
+# print(3==3.000000000000000001)
+
+# es diferente a
+# print(4!=3)
+# print(False!=True)
+
+#es mayor que
+#>
+
+#es menor que
+#<
+
+#es mayor o igual que, es menor o igual que
+#>=
+#<=
+
+'''
+Operadores de asignacion
+'''
+# x=20
+# x=20+1
+# print(x)
+
+#Incremento
+# x+=2
+# print(x)
+
+#Decrecimiento
+# y=5 
+# y=y-1
+# y-=1
+# print(y)
+
+# Asigna multiplicacion
+
+# Asigna division
+
+# Asigna modulo
+
+# Asigna exponente
+
+'''Operadores logigos
+and: exige que todas las condiciones sean True para que el resultado sea True, de lo contrario sera False
+or: exige que al menos una de las condiciones sea True para que el resultado sea True
+not: Se valida si una variable es Falses o True, si la variable existe en memoria tenemos un valor por defecto de True
+'''
+# x=4 #Defino el valor de x
+# x==4 #Pregunto si x es igual a 4
+# y=4
+# print(x==4 and y==8)
+
+#uso del operador or
+# print(x==100 or y==4)
+
+#uso del operador not
+# print(not x)
+# enunciado=False
+# print(not enunciado)
+
+#usuario_autenticado=True
+#boton_cerrar_sesion=True
+#print(not usuario_autenticado) 
