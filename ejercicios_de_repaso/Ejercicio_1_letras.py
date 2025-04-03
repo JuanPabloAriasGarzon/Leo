2c2ee0593766b8712ab363e84ccbedbc76a58dde
JuanPabloAriasGarzon <jupaga402@gmail.com>
2025-04-02 19:33:32 -0500

    Ejercicio 1 letras

---
+++ ejercicios_de_repaso/Ejercicio_1_letras.py
@@ -0,0 +1,9 @@
+'''
+Ejercicio 1: Contar Letras en una Palabra
+
+Problema:
+Solicitar al usuario una palabra y contar cuántas letras tiene.
+'''
+
+palabra=input("Ingrese la palabra:")
+print(len(palabra))
