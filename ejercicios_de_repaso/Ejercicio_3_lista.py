11f7303b7bc82b8c6911bd4982787a8cb6f5a39c
JuanPabloAriasGarzon <jupaga402@gmail.com>
2025-04-03 01:45:50 -0500

    Ejercicio 3 lista

---
+++ ejercicios_de_repaso/Ejercicio_3_lista.py
@@ -0,0 +1,14 @@
+'''
+Ejercicio 3: Buscar una Palabra en una Lista
+
+Problema:
+Crear una lista de palabras predefinida y pedir al usuario una palabra. 
+Indicar si está en la lista o no.
+'''
+
+mi_lista=["tiburon","pez","agua","ballena"]
+
+for elemento in mi_lista["camaron"]:
+    mi_lista.append(elemento)
+
+print(mi_lista)
