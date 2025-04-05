54cfbec7d59748705cbb52ca42b38b4041c3d189
JuanPabloAriasGarzon <jupaga402@gmail.com>
2025-04-04 19:09:14 -0500

    Ejercicio docs

--- ejercicios_de_repaso/Ejercicio_docs.py
+++ ejercicios_de_repaso/Ejercicio_docs.py
@@ -1,5 +1,11 @@
-for n in range(2, 10):
-    for x in range(2, n):
-        if n % x == 0:
-            print(f"{n} es divisible por {x} * {n//x}")
-            break
+# for n in range(2, 10):
+#     for x in range(2, n):
+#         if n % x == 0:
+#             print(f"{n} es divisible por {x} * {n//x}")
+#             break
+
+for num in range(2, 10):
+    if num % 2 == 0:
+        print(f"Found an even number {num}")
+        continue
+    print(f"Found an odd number {num}")
